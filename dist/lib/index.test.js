5d18e0c107e7baa411fe874d5bb97ade961ed842
Radinvafaei <radin.vafaei@icloud.com>
2025-02-26 15:49:53 +0330

    implement test and build infrastructure

---
+++ dist/lib/index.test.js
@@ -0,0 +1,8 @@
+"use strict";
+describe('test', ()=>{
+    it('Should return true', ()=>{
+        expect(true).toBe(true);
+    });
+});
+
+//# sourceMappingURL=index.test.js.map
---
+++ lib/index.test.ts
@@ -0,0 +1,5 @@
+describe('test', () => {
+    it('Should return true', () => {
+        expect(true).toBe(true);
+    })
+})
